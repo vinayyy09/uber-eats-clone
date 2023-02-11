002f502d98aaa37bd965b6ab97b833a65346b671
Vinay Babar <vinaybabar09@gmail.com>
2023-02-11 13:52:44 +0530

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* card */}
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="/"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/3535383/pexels-photo-3535383.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="/"
+        />
+      </div>
+      <div className="rounded-xl relative">
+        {/* overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Trending Places</p>
+          <p className="px-2">Top 10</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
